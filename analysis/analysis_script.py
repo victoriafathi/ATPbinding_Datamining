820626e25764f28c325dbe1c59efffb0f43af43d
Victoria FATHI <victoria.fathi@gmail.com>
2021-04-27 16:45:17 +0200

    Display attempt

--- analysis/analysis_script.py
+++ analysis/analysis_script.py
@@ -4,6 +4,8 @@
 
 import csv
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import export_graphviz
+
 #from sklearn.datasets import make_classification
 
 file = "../data.preparation/matrix_ind_var.tsv"
@@ -12,22 +14,17 @@ source = csv.reader(open(file, 'r', encoding='utf-8'),delimiter='\t')
 data = []
 to_classify = []
 
-source.__next__()
+header = source.__next__()
 
 for row in source:
-    #rowtemp = [el for el in row]
-    #for i in range(1,len(rowtemp) -1):
-    #    rowtemp = float(rowtemp[i])
-    #rowtemp = bool(rowtemp[-1])
-    
     if row[-1] == "*":
-        to_classify.append(row[1:-1])
+        to_classify.append(row[1:-1]) #data to classify
     else:
-        data.append(row)
+        data.append(row) #training data
 
 
 X = [line[1:-1] for line in data]
-y = [line[-1] for line in data]
+y = [line[-1] for line in data] # classes
 
 print(X[0])
 
@@ -36,4 +33,18 @@ print(X[0])
 
 classifier = RandomForestClassifier()
 classifier.fit(X,y)
+test = classifier.estimators_[5]
+
+export_graphviz(test, 
+                out_file='tree.dot', 
+                feature_names = header[1:-1],
+                rounded = True, proportion = False, 
+                precision = 2, filled = True)
+
+from subprocess import call
+call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png', '-Gdpi=600'])
+
+from IPython.display import Image
+Image(filename = 'tree.png')
+
 print(classifier.predict(to_classify))
